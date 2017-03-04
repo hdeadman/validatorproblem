20c791691f276fc06b4a29d40424381fed0dbd68
Onur Kağan Özcan <onurkaganozcan@gmail.com>
2017-03-05 00:16:34 +0300

    first commit

---
+++ src/main/java/com/okohub/ValidatorproblemApplication.java
@@ -0,0 +1,12 @@
+package com.okohub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidatorproblemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidatorproblemApplication.class, args);
+	}
+}
